326575403d27ddfb60b128c6cae072c5d994f087
virupaksha18 <virupaksha755@gmail.com>
2025-07-17 12:15:01 +0530

    Connected frontend to Render backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:['http://localhost:3000','https://nitpulse.vercel.app'],
+  origin:['https://nitpulse.vercel.app'],
   methods:['GET','POST','DELETE'],
   credentials:true
 }));
