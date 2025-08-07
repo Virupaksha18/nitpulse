15767e62b466715045de9115f7320104f7add239
virupaksha18 <virupaksha755@gmail.com>
2025-08-07 15:21:08 +0530

    Connected frontend to Render backend

--- backend/routes/huggingface.js
+++ backend/routes/huggingface.js
@@ -11,7 +11,7 @@ router.post('/generate-text', async (req, res) => {
     const userInput = req.body.text;
 
     const response = await axios.post(
-      'https://api-inference.huggingface.co/models/gpt2', // You can change to another model
+      'https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct', // You can change to another model
       {
         inputs: userInput
       },
