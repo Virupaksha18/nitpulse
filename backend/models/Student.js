327bdc9007d0eb9ce7d0682b7dec197b5a83637e
virupaksha18 <virupaksha755@gmail.com>
2025-08-09 06:14:13 +0530

    Connected frontend to Render backend

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -4,7 +4,7 @@ const studentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   usn: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  sem: { type: String, required: true },
+  semester: { type: String, required: true },
   branch: { type: String, required: true },
 }, { timestamps: true });
 
