0f015c6c232aa3eb27f64fe443be0f4c8191a87a
virupaksha18 <virupaksha755@gmail.com>
2025-08-09 19:06:26 +0530

    Connected frontend to Render backend

--- nitpulse/src/index.js
+++ nitpulse/src/index.js
@@ -4,17 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
-import { BrowserRouter } from 'react-router-dom';  // <-- import this
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>   {/* <-- wrap your app here */}
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
 
