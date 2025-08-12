bf2868015b772f2ae76fca6f0eaa860ab16d3e6f
virupaksha18 <virupaksha755@gmail.com>
2025-08-12 15:48:57 +0530

    Connected frontend to Render backend

--- nitpulse/src/pages/HomePage.jsx
+++ nitpulse/src/pages/HomePage.jsx
@@ -344,7 +344,7 @@ if(!user){
       </div>
 
       {/* More Features */}
-      <div className="max-w-6xl mx-auto mb-8">
+      <div className="max-w-6xl mx-auto mb-3">
         <h2 className="text-3xl font-bold text-center mb-10 text-gray-800">More Features</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
 
