70703066b9ee1e419f44cfe622bca4e2fa2f7a59
virupaksha18 <virupaksha755@gmail.com>
2025-08-30 22:24:10 +0530

    Connected frontend to Render backend

--- nitpulse/src/index.js
+++ nitpulse/src/index.js
@@ -14,5 +14,32 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
+if ("serviceWorker" in navigator){
+  window.addEventListener("load",() => {
+    navigator.serviceWorker.register("/service-worker.js").then((registration) =>{
+      console.log("Service Worker registerd:", registration);
+    })
+    .catch((error) => {
+      console.log("Service Worker registration failed:",error);
+    });
+  });
+  let deferredPrompt;
+  window.addEventListener("beforeinstallprompt", (e) =>{
+    e.preventDefault();
+    deferredPrompt = e;
+    console.log("beforeinstallprompt event fired");
+    const installBtn = document.getElementById("installBtn");
+    if (installBtn) {
+      installBtn.style.display ="block";
+      installBtn.addEventListener("click", async ()=> { 
+        installBtn.style.display = "none";
+        deferredPrompt.prompt();
+        const {outcome } = await deferredPrompt.userChoice;
+        console.log(`User response: $ {outcome}`);
+        deferredPrompt = null;
 
+      });
+    }
+  });
+}
 reportWebVitals();
