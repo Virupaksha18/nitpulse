835eb7273c7d6edf90deec7639fb75fb3c58d16a
virupaksha18 <virupaksha755@gmail.com>
2025-09-01 22:45:29 +0530

    Connected frontend to Render backend

--- nitpulse/src/components/CSE/2022/7th/AssignmentsDetails.jsx
+++ nitpulse/src/components/CSE/2022/7th/AssignmentsDetails.jsx
@@ -58,15 +58,26 @@ const AssignmentsDetails = () => {
       <h2 className="text-3xl font-bold text-center mb-10">{subject.title} - Assignments</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        {subject.assignments.map((item, index) => (
+        {subject.assignments.map((item, index) => {
+          const pdfUrl = `${Process.env.REACT_APP_BASE_URL}/files/assignments/CSE/2022/7th/${subjectSlug}/assignment${index + 1}.pdf`;
+          return(
           <div
             key={index}
             className="bg-black text-white p-6 rounded-xl shadow-md hover:bg-gray-900 transition duration-300"
           >
             <h3 className="text-lg font-semibold mb-2">Assignment {index + 1}</h3>
             <p className="text-sm text-gray-300">{item}</p>
-          </div>
-        ))}
+            <a
+            href={pdfUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+          >
+            Download PDF
+            </a>
+            </div>
+        );
+      })}
       </div>
     </div>
   );
